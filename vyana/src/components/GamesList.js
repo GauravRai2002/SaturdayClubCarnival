875756a10f1c7e3d1644d5b94b1cd40dd3565670
GauravRai2002 <88336754+GauravRai2002@users.noreply.github.com>
2023-05-02 15:36:29 +0530

    Schedule update

--- vyana/src/components/GamesList.js
+++ vyana/src/components/GamesList.js
@@ -3,16 +3,29 @@ import './GamesList.css'
 import { Link } from 'react-router-dom'
 
 function GamesList() {
+
+    // var gameToggle = document.getElementsByClassName('game')
+    var i
+    setTimeout(() => {
+        for (i = 0; i < 7; i++) {
+            document.getElementsByClassName('game')[i].addEventListener('click', event => {
+                document.getElementsByClassName('active')[0].classList.remove('active');
+                event.target.parentElement.classList.add('active')
+            }, 500);
+        }
+    })
+
+
     return (
         <>
             <ul className='games-list'>
-                <li className='game'>  <Link to='badminton' className='link'> Badminton</Link></li>
-                <li className='game'>  <Link to='tabletennis' className='link'>Table Tennis</Link></li>
-                <li className='game'> <Link to='snooker' className='link'>Snooker</Link></li>
-                <li className='game'> <Link to='tennis' className='link'>Tennis -Finals</Link></li>
-                <li className='game'> <Link to='squash' className='link'>Squash</Link></li>
-                <li className='game'> <Link to='swimming' className='link'>Swimming</Link></li>
-                <li className='game'> <Link to='darts' className='link'>Darts</Link></li>
+                <li className='game active' >  <Link to='badminton' className='link'> Badminton</Link></li>
+                <li className='game' >  <Link to='tabletennis' className='link'>Table Tennis</Link></li>
+                <li className='game' > <Link to='snooker' className='link'>Snooker</Link></li>
+                <li className='game' > <Link to='tennis' className='link'>Tennis -Finals</Link></li>
+                <li className='game' > <Link to='squash' className='link'>Squash</Link></li>
+                <li className='game' > <Link to='swimming' className='link'>Swimming</Link></li>
+                <li className='game' > <Link to='darts' className='link'>Darts</Link></li>
             </ul>
         </>
     )
